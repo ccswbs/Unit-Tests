05b61e5e742dd3783b7ef12e351f790cea22d219
enichol <enicho2@mail.uoguelph.ca>
2015-11-19 12:55:27 -0500

    ^

--- pullNodes.sh
+++ pullNodes.sh
@@ -8,5 +8,5 @@ if [ $# -lt 4 ]; then
 	exit
 fi
 
-# This one's a doosy:         SQL query to the server to select nodes    pipes to sed to add 1st URL     pipes to sed to add second url     strips first line (column names) then redirects to text file
-drush @aoda.$1.dev sql-query "SELECT sourceid1,destid1 from "$1"."$2"" | sed -e 's_[0-9]*_'$3'/node&_' | sed -e 's_[0-9]*$_ '$4'/node&_' | sed -n '2,$p' > nodelists/$1-nodelist.txt
+# This one's a doosy:         SQL query to the server to select nodes    pipes to sed to add 1st URL       pipes to sed to add second url     strips first line (column names) then redirects to text file
+drush @aoda.$1.dev sql-query "SELECT sourceid1,destid1 from "$1"."$2"" | sed -e 's_^[0-9]*_'$3'/node/&_' | sed -e 's_[0-9]*$_ '$4'/node/&_' | sed -n '2,$p' > nodelists/$1-nodelist.txt
