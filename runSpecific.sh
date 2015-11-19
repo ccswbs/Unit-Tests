6d3512f869c9dd4ef90ae6a9cbabb2cd5e075a09
enichol <enicho2@mail.uoguelph.ca>
2015-11-19 13:59:04 -0500

    Adding nodelists for the unit test demo

--- runSpecific.sh
+++ runSpecific.sh
@@ -11,7 +11,7 @@ if [ $# -lt 3 ]; then
 	exit
 fi
 # Checks that the nodelist file specified exists
-if [ ! -f $3 ]; then
+if [ ! -f nodelists/$3 ]; then
 	echo -e "\e[31mERROR\e[0m"
 	echo "File '" $3 "' not found! Make sure the name of the nodelist file is correct and the path to it is specified."
 	exit
@@ -23,3 +23,4 @@ while read line
 do 
 	node nightwatch --test tests/$1/$2 $line
 done < nodelists/$3 
+node nightwatch --test tests/$1/$2 $line # runs the command one extra time for the final line
