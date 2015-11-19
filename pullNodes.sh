a7f2cb094d568e2a57fbc71870550dfd6776f98c
enichol <enicho2@mail.uoguelph.ca>
2015-11-19 10:41:40 -0500

    touch ups

--- pullNodes.sh
+++ pullNodes.sh
@@ -4,7 +4,7 @@ RED='\033[0:31m'
 
 if [ $# -lt 2 ]; then
 	echo -e "\e[31mERROR\e[0m"
-	echo "Usage: ./pullNodes [sitename] [migration table name]"
+	echo "Usage: ./pullNodes [db_name] [migration_table_name] [base_url]"
 fi
 
 drush @aoda.$1.dev sql-query "SELECT sourceid1,destid1 from "$1"."$2"" | sed -n '2,$p' > nodelists/$1-nodelist.txt # sed cuts out column headers and writes it to a file
