325c0dd590b05a1e6e64abcfb788fc0c57e45201
enichol <enicho2@mail.uoguelph.ca>
2015-11-19 14:04:03 -0500

    updating to my version of pullNodes.sh

--- pullNodes.sh
+++ pullNodes.sh
@@ -8,5 +8,14 @@ if [ $# -lt 4 ]; then
 	exit
 fi
 
-# This one's a doosy:         SQL query to the server to select nodes    pipes to sed to add 1st URL       pipes to sed to add second url     strips first line (column names) then redirects to text file
-drush @aoda.$1.dev sql-query "SELECT sourceid1,destid1 from "$1"."$2"" | sed -e 's_^[0-9]*_'$3'/node/&_' | sed -e 's_[0-9]*$_ '$4'/node/&_' | sed -n '2,$p' > nodelists/$1-nodelist.txt
+# This one's a doosy:            
+#1) DRUSH command to select the correct database and table from SQL server
+#2) pipes to sed to add 1st URL
+#3) pipes to sed to add second URL
+#4) strips first line (SQL column names)
+#5) then redirects to text file
+drush @aoda.$1.dev sql-query "SELECT sourceid1,destid1 from "$1"."$2"" \
+| sed -e 's_^[0-9]*_'$3'/node/&_' \
+| sed -e 's_[0-9]*$_ '$4'/node/&_' \
+| sed -n '2,$p' \
+> nodelists/$1-nodelist.txt
